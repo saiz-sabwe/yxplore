2c03c8a9f37c7df0280701cdf113913aad5a75b3
joyce sabwe <joycesabwe@gmail.com>
2025-03-19 18:24:55 +0100

    change setting

--- ModuleHotels/views.py
+++ ModuleHotels/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    context = {
+        "title": 'titre de la vue'
+    }
+    return render(request, "backoffice/index.html", context)
--- yxplore/urls.py
+++ yxplore/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("backoffice.urls")),
+    path("", include("ModuleHotels.urls")),
     path('admin/', admin.site.urls),
 ]
