257c1e5ed8996dcce597612718a80477e09d7a5a
saiz-sabwe <76713037+saiz-sabwe@users.noreply.github.com>
2025-08-28 13:49:09 +0100

    Update deploy workflow and switch to PostgreSQL
    Renamed deploy workflow and job names to reference YXplore, updated environment variable names for deployment directories, and added container verification step. Switched Django database backend from SQLite to PostgreSQL with new connection settings in settings.py.

--- YXPLORE_NODE/settings.py
+++ YXPLORE_NODE/settings.py
@@ -105,10 +105,21 @@ ASGI_APPLICATION = 'YXPLORE_NODE.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bright_db',
+        'USER': 'bright_user',
+        'PASSWORD': 'bright_pass',
+        'HOST': 'pgsql',
+        'PORT': '5432',
     }
 }
 
